61081f2e133b0724eed889168204c968fc1fd415
Neil Larion and Tim Durward <neil.larion@gmail.com>
2016-01-20 11:00:17 -0800

    Refactor addAy to use vowelDector

--- js/master.js
+++ js/master.js
@@ -10,13 +10,10 @@ function vowelDector(char){
 }
 
 function addAy(word){
-  var vowels = ['a','e','i','o','u','A', 'E', 'I', 'O', 'U'];
-  for(var i = 0; i < vowels.length; i++){
-    if(word.charAt(0).toString() == vowels[i].toString()){
-      //console.log("true");
-      word = word.concat("ay");
-      return word;
-    }
+  if(vowelDector(word[0])){
+    //console.log("true");
+    word = word.concat("ay");
+    return word;
   }
 }
 
