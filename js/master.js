be6544773520df63d92d24dbdc998363795801ca
Neil Larion and Tim Durward <timothy.a.durward@gmail.com>
2016-01-20 10:36:33 -0800

    add consonant logic

--- js/master.js
+++ js/master.js
@@ -1,3 +1,14 @@
+function vowelDector(char){
+  var vowels = ['a','e','i','o','u','A', 'E', 'I', 'O', 'U'];
+  for(var i = 0; i < vowels.length; i++){
+    if(char.charAt(0).toString() == vowels[i].toString()){
+      console.log("true");
+      return true;
+    }
+  }
+  return false;
+}
+
 function addAy(word){
   var vowels = ['a','e','i','o','u','A', 'E', 'I', 'O', 'U'];
   for(var i = 0; i < vowels.length; i++){
@@ -7,4 +18,35 @@ function addAy(word){
       return word;
     }
   }
+}
+
+function consAy(word){
+  var consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','R','S','T','V','W','X','Y','Z'];
+  word = word.split("");
+  console.log(word);
+  var arr = [];
+  for (var i = 0; i< word.length; i++){
+    console.log(word[i]);
+    if(vowelDector(word[i])){
+      //do something
+      break;
+    }else{
+      for (var j = 0; j < consonants.length; j++){
+        if(word[i]===consonants[j]){
+          //do something
+          console.log("we got a match: "+consonants[j]);
+          arr.push(word[i]);
+        }
+      }
+    }
+  }
+  console.log(arr);
+  for (var i = 0; i < arr.length; i++) {
+
+    word.push(arr[i]);
+    word.shift();
+  }
+  console.log(word);
+  console.log(addAy(word.join('')));
+  return addAy(word.join(''));
 }
--- spec/spec.js
+++ spec/spec.js
@@ -3,3 +3,17 @@ describe('addAy',function(){
     expect(addAy("open")).to.equal("openay");
   });
 });
+
+describe('consAy',function(){
+  it("Will add first consecutive consonants to end of the word & add 'ay'",function(){
+    expect(consAy("starbucks")).to.equal("arbucksstay");
+  });
+});
+
+describe('vowelDector',function(){
+  it("Will add first consecutive consonants to end of the word & add 'ay'",function(){
+    expect(vowelDector("a")).to.equal(true);
+  });
+});
+
+//For words that start with one or more consonants, move all of the first consecutive consonants to the end and add "ay". (If the first consonants include "qu", move the "u" along with the "q". Don't forget about words like "squeal" where the "qu" doesn't come first!)
